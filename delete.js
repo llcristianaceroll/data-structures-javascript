b3515cb02f6f0cc1f7a198d2d6ca760c4c367e9c
Cristian Acero <llcristianaceroll@gmail.com>
2022-07-28 15:05:45 -0500

    delete method created =)

---
+++ delete.js
@@ -0,0 +1,48 @@
+class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index] 
+
+    }
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftIdex(index);
+        return item;
+    }
+    shiftIdex(index) {
+        for(let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        console.log(this.data)
+        delete this.data[this.length - 1];
+        this.length--;
+    }   
+        
+}
+
+const myArray = new MyArray();
+
+myArray.push("Cristan");
+myArray.push("Acero");
+myArray.push("Leona");
+myArray.push("Liu");
+
+console.log(myArray.delete(0));
+console.log(myArray.data);
+console.log(myArray);
